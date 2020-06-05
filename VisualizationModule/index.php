733dd1495b765d39fa17d66a1f87907af981030e
KimKyuRi <heavenade28@gmail.com>
2020-06-05 18:06:23 +0900

    ADD: Bootstrap Lib 및 Resource Image

---
+++ VisualizationModule/index.php
@@ -0,0 +1,17 @@
+<!-- 메인페이지 -->
+<html>
+  <head>
+    <meta http-equiv="content-type" content = "text/html; charset = UTF-8" />
+    <title> 시각화모듈 어쩌구 </title>
+  </head>
+  <body>
+
+    <?
+    ini_set("display_errors", 1);
+
+    include "../lib/db_connect.php";//위치 수정
+    $connect = dbconn();
+    ?>
+
+  </body>
+</html>
