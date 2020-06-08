2436bd0a187a3877e7e2f9026bf23dee910811e0
KimKyuRi <heavenade28@gmail.com>
2020-06-09 03:30:05 +0900

    ADD: 메인 결과 페이지 UI 틀 작업
    - Visualization, Statistics의 마인드맵, 그래프 제외 뼈대를 작업

--- VisualizationModule/search/search_result.php
+++ VisualizationModule/search/search_result.php
@@ -2,32 +2,30 @@
 <html>
   <head>
     <title> Visualization Module </title>
-    <!--Meta -->
     <meta http-equiv="content-type" content = "text/html; charset = UTF-8" />
     <meta name="viewport" content="width=device-width; initial-scale=1">
-    <!-- 부트스트랩 사용 -->
     <link rel="stylesheet" href="../css/bootstrap.css">
     <link rel="stylesheet" href="../css/capstone-custom.css">
-    <!-- 스타일 변경: 왜 부트스트랩에서 안먹힘  -->
   </head>
 
   <body>
-    <!-- php -->
     <?
     ini_set("display_errors", 1);
-    header("content-type:text/html; charset=UTF-8");
-    
+    header("content-type:text/html; charset=UTF-8");   
     include_once "../lib/db_connect.php";//db연결
-    include_once "../lib/rcmd_random.php";//recommend random
-
-    //이미 검색된 검색어
+    //검색한 문자열 받기
     $result_text = "";
-
-    //검색한 데이터 받기
-    if($_POST['search_text'])
-    {
-        $result_text=$_POST['search_text'];  
-    }
+    if($_POST['search_text']){ $result_text=$_POST['search_text'];  }
+    
+    /*변수*/
+    //Main Fearture
+    $positive_feature_text="카메라 인덕션";
+    $negative_feature_text="배터리 폭발";
+    
+    //Detailed Filter
+    
+    /*함수*/
+    //get_productdata.php 참고
     ?>
    
     <!-- 컨테이너 -->
@@ -48,7 +46,6 @@
           <div class="col-lg-5 col-md-2 col-sm-2 col-xs-2">
           </div>
       </div>
-
       <!-- SearchPanel-->
       <div class="row">
         <div class="col-lg-6 col-md-8 col-sm-8 col-xs-8">
@@ -62,9 +59,154 @@
           </div>
         </div>
       </div>
-      <hr><br><br>
-    </div>
+      <hr>
+      <!-- Main Feature -->
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-8">
+          <div class = "main-feature center-block">
+            <span class="main-feature-text center-block"><?=$result_text?>의 대표적인 특징은<br>
+            <?="     $positive_feature_text     |     $negative_feature_text    "?><br>입니다!</span>
+          </div>
+        </div>
+        <div class="col-md-2"></div>
+      </div>
+      <hr>
+
+
+      <!-- Detailed Filter -->
+      <div class="detailed-filter row">
+        <div class="col-md-12"><span class="detailed-filter-text">DETAILED FILTER</span>
+          <div class="row">
+              <div class="col-sm-2">
+              </div>
+              <div class="col-sm-8">
+              <br><br>
+                  <!-- card grid  -->
+                  <div class="row">
+                    <div class="clearfix visible-sm-block"></div>
+                    <div class="col-md-3 col-sm-4 col-xs-6">
+                      <div class ="detailed-filter-card center-block">
+                      <img src="../resources/Filter/1.svg">
+                      </div>
+                    </div>
+                    <div class="col-md-3 col-sm-4 col-xs-6">
+                      <div class="detailed-filter-card center-block">
+                      <img src="../resources/Filter/1.svg">
+                      </div>
+                    </div>
+                    <div class="col-md-3 col-sm-4 col-xs-6">
+                      <div class="detailed-filter-card center-block">
+                      <img src="../resources/Filter/1.svg">
+                      </div>
+                    </div>
+                    <div class="col-md-3 col-sm-4 col-xs-6">
+                      <div class="detailed-filter-card center-block">
+                      <img src="../resources/Filter/1.svg">
+                      </div>
+                    </div>
+                    <div class="col-md-3 col-sm-4 col-xs-6">
+                      <div class ="detailed-filter-card center-block">
+                      <img src="../resources/Filter/1.svg">
+                      </div>
+                    </div>
+                    <div class="col-md-3 col-sm-4 col-xs-6">
+                      <div class ="detailed-filter-card center-block">
+                      <img src="../resources/Filter/1.svg">
+                      </div>
+                    </div>
+                    <div class="col-md-3 col-sm-4 col-xs-6">
+                      <div class ="detailed-filter-card center-block">
+                      <img src="../resources/Filter/1.svg">
+                      </div>
+                    </div>
+                    <div class="col-md-3 col-sm-4 col-xs-6">
+                      <div class ="detailed-filter-card center-block">
+                      <img src="../resources/Filter/1.svg">
+                      </div>
+                    </div>
+                    <div class="col-md-3 col-sm-4 col-xs-6">
+                      <div class = "detailed-filter-card center-block">
+                      <img src="../resources/Filter/1.svg">
+                      </div>
+                    </div>
+                    <div class="col-md-3 col-sm-4 col-xs-6">
+                      <div class = "detailed-filter-card center-block">
+                      <img src="../resources/Filter/1.svg">
+                      </div>
+                    </div>
+                    <div class="col-md-3 col-sm-4 col-xs-6">
+                      <div class = "detailed-filter-card center-block">
+                      <img src="../resources/Filter/1.svg">
+                      </div>
+                    </div>
+                    <div class="col-md-3 col-sm-4 col-xs-6">
+                      <div class = "detailed-filter-card center-block">
+                      <img src="../resources/Filter/1.svg">
+                      </div>
+                    </div>
+                  </div>       
+              </div>
+              <div class="col-sm-2"></div>
+          </div>
+        </div>
+      </div>
+      <hr>
+      
+      <!-- Visualization -->
+      <div class="visualization row">
+          <div class="col-md-12"><span class="visualization-text"><br>VISUALIZATION</span>
+          <br><br><br><br><br><br>
+            <div class="row">
+                  <div class="clearfix visible-sm-block"></div>
+                  <div class="col-sm-4">
+                      <div class ="visualization-cate center-block">
+                      <img src="../resources/Category Resize.svg">
+                      </div>
+                  </div>
+                  <div class="col-sm-8">
+                      <div class ="visualization-mindmap center-block">
+                        <!-- 마인드 맵 -->
+                      </div>
+                  </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr>
+
+      <!-- Statistics -->
+      <div class="visualization row">
+          <div class="col-md-12"><span class="visualization-text"><br>STATISTICS</span>
+          <br><br><br><br><br><br>
+            <div class="row">
+                  <div class="clearfix visible-sm-block"></div>
+                  <div class="col-sm-4">
+                      <div class ="visualization-cate center-block">
+                      <img src="../resources/Category Resize.svg">
+                      </div>
+                  </div>
+                  <div class="col-sm-8">
+                      <div class ="visualization-mindmap center-block">
+                        <!-- 마인드 맵 -->
+                      </div>
+                  </div>
+            </div>
+          </div>
+        </div>
+        <hr>
+
+
+
+
+
+
+      </div>
     
+
+
+
+
     <!-- js 사용 -->
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script type="text/javascript" src="../js/bootstrap.js"></script>
